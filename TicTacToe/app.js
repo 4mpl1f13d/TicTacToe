5313aa0ec0e6175c39ea5079bce62c029ad3ba78
4mpl1f13d <4mpl1f13d@users.noreply.github.com>
2020-01-15 15:43:11 -0600

    Add files via upload

---
+++ TicTacToe/app.js
@@ -0,0 +1,11 @@
+
+var val = substract (add(2,3) , substract(5, 3)) ;
+console.log(val);
+
+function add (num1, num2){
+    return num1 + num2;
+}
+
+function substract (num1, num2){
+    return num1 - num2;
+}
